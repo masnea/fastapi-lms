a46ad3ac2f6072df290fd028f57f21f9e6a2eae8
masnea <m.andavari@gmail.com>
2022-02-19 01:45:53 +0330

    alembic

---
+++ db/db_setup.py
@@ -0,0 +1,26 @@
+from asyncio import futures
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://nimlms:nimlms@localhost/fast_lms"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}, future=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
+
+Base = declarative_base()
+
+# DB utilities
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
